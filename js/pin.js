6370dc2776a302229ce3a96d1bb2b7bf386bb210
andryginx <andryginx@gmail.com>
2025-02-15 13:39:50 +0200

    Update pin.js

--- js/pin.js
+++ js/pin.js
@@ -2,7 +2,7 @@ function checkPin() {
     const pin = document.getElementById("pinInput").value;
     if (pin === "1602") {
         localStorage.setItem("access", "granted");
-        window.location.href = "index.html";
+        window.location.href = "pin.html";
     } else {
         document.getElementById("error").textContent = "Неверный PIN-код!";
     }
